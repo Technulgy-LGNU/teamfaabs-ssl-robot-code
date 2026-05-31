6a582285c2b3367695a1eaa508aa2414adeb4627
Elias Braun <braunelias@tghd.email>
2026-05-31 13:26:17 +0200

    smoother acceleration

--- tf_jetsoncode/src/robot_logic/helpers.rs
+++ tf_jetsoncode/src/robot_logic/helpers.rs
@@ -241,7 +241,11 @@ pub(crate) fn raw_move_towards(
 
 #[inline]
 pub(crate) fn raw_movement_accel(dist: f32) -> f32 {
-  (dist * 3f32).clamp(60f32, RAW_MAX_SPEED_MM_S).round()
+  let ratio = (dist * 3f32 / RAW_MAX_SPEED_MM_S).clamp(0f32, 1f32);
+
+  let smooth = ratio * ratio * (3f32 - 2f32 * ratio); // smoothstep
+
+  60f32 + smooth * (RAW_MAX_SPEED_MM_S - 60f32)
 }
 
 mod test {
